79ffe22804b63f551b2ab00df0f971afc49be169
Grzegorz Wojcicki <grzegorz-wojcicki@outlook.com>
2015-04-26 23:59:15 +0200

    laptop -> PC

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ questions/comments: grzegorz-wojcicki@outlook.com
 
 ```bash
 # unpack/run neo4j
+aptitude install libblas-dev liblapack-dev gfortran g++
 virtualenv alchemy-clustering
 cd alchemy-clustering
 source bin/activate
@@ -21,4 +22,15 @@ pip install -r requirements.txt
 # change config however you like
 python2 app.py
 # done
+```
+
+```neo4j
+
+WITH ["audi", "bmw", "form", "mercedes", "nissan", "opel", "peugeot", "renault", "toyota", "wolksvagen"]
+AS names FOREACH (r IN range(0,20000) | CREATE (:Car {id:r, name:names[r % size(names)]+" "+r}));
+
+WITH ["engine", "wheels", "dashboard", "speedometer", "sunroof", "alarm", "abs", "esp", "asr", "muffler"]
+AS names FOREACH (r IN range(0,100) | CREATE (:Part {id:r, name:names[r % size(names)]+" "+r}));
+
+// Added 20001 labels, created 20001 nodes, set 40002 properties, statement executed in 20211 ms.
 ```
--- app.py
+++ app.py
@@ -1,9 +1,23 @@
+# -*- coding: utf-8 -*-
 __author__ = 'rikkt0r'
 
 from flask import Flask, render_template
 from py2neo import Graph, Node, Relationship, authenticate
 import os
 
+
+# TODO
+"""
+Alchemy.js - clusterization and visualization of neo4j db
+http://nicolewhite.github.io/2014/07/24/visualize-subset-neo4j-alchemy.html
+http://graphalchemist.github.io/Alchemy/#/examples
+http://stackoverflow.com/questions/1545606/python-k-means-algorithm
+Dla zbioru 20000x20 zaproponować własny use case, np. w oparciu o metodę klasteryzacji k-means
+lub inną z bibliotek pythonowskich.
+
+https://www.cs.ucsb.edu/~xyan/classes/CS595D-2009winter/MCL_Presentation2.pdf
+Erdős–Rényi model
+"""
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -19,6 +33,12 @@ def index():
     print test
 
     return render_template('index.html', txt=test)
+
+@app.route('generate/<int:modifier>')
+def generate(modifier=0):
+
+    if modifier:
+        pass
 
 
 @app.route('/mockup')
