1562de9a771369f976cf345120c939be09dd40ee
Grzegorz Wójcicki <grzegorz-wojcicki@outlook.com>
2015-04-28 01:43:39 +0200

    Working version

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ questions/comments: grzegorz-wojcicki@outlook.com
 
 ```bash
 # unpack/run neo4j
-aptitude install libblas-dev liblapack-dev gfortran g++
+aptitude install libblas-dev liblapack-dev gfortran g++ libigraph-dev  
 virtualenv alchemy-clustering
 cd alchemy-clustering
 source bin/activate
@@ -31,21 +31,18 @@ MATCH (n)
 OPTIONAL MATCH (n)-[r]-()
 DELETE n,r
 
+// Added 500 labels, created 500 nodes, set 1000 properties, statement executed in 127 ms.
 WITH ["audi", "bmw", "ford", "mercedes", "nissan", "opel", "peugeot", "renault", "toyota", "volkswagen"]
-AS names FOREACH (r IN range(0,20000) | CREATE (:Car {id:r, name:names[r % size(names)]+" "+r}));
+AS names FOREACH (r IN range(0,499) | CREATE (:Car {id:r, name:names[r % size(names)]+" "+r}));
 
-// Added 20001 labels, created 20001 nodes, set 40002 properties, statement executed in 632 ms.
-
+// Added 10 labels, created 10 nodes, set 20 properties, statement executed in 41 ms.
 WITH ["hybrid engine", "spare wheel", "computer", "digital speedometer", "sunroof", "alarm", "abs", "esp", "asr", "muffler"]
 AS names FOREACH (r IN range(0,9) | CREATE (:Part {id:r, name:names[r % size(names)]+" "+r}));
 
-// Added 101 labels, created 101 nodes, set 202 properties, statement executed in 65 ms.
-
+// Created 143 relationships, statement executed in 112 ms.
 MATCH (c:Car),(p:Part) WITH c,p
-LIMIT 150000 WHERE rand() < 0.1
+LIMIT 5000 WHERE rand() < 0.1
 CREATE (c)-[:HAS]->(p);
-
-// Created 15136 relationships, statement executed in 659 ms.
 
 create index on :Car(id);
 // Added 1 index, statement executed in 36 ms.
@@ -57,5 +54,13 @@ create index on :Part(id);
 MATCH (p:Part)<-[r:HAS]-(c:Car) WHERE p.id = 9 OR c.id = 100 OR c.id = 111 RETURN r; // duzo!
 MATCH (p:Part)<-[r:HAS]-(c:Car) WHERE c.id IN [10,15,22,545,346,500,123,54,78,456,345,333,444,222,111] RETURN r LIMIT 100;
 
-
+MATCH (c)-[b:HAS]->(p) RETURN b, rand() as r ORDER BY r
 ```
+
+### Links:
+* http://arxiv.org/abs/0908.1062
+* http://nicolewhite.github.io/2014/07/24/visualize-subset-neo4j-alchemy.html
+* http://graphalchemist.github.io/Alchemy/#/examples
+* http://stackoverflow.com/questions/1545606/python-k-means-algorithm
+* https://www.cs.ucsb.edu/~xyan/classes/CS595D-2009winter/MCL_Presentation2.pdf
+* http://jexp.de/blog/2014/03/quickly-create-a-100k-neo4j-graph-data-model-with-cypher-only/
--- app.py
+++ app.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
 __author__ = 'rikkt0r'
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for, redirect
 import py2neo
 import igraph
 import os
-from random import randrange
 
-
-
-# TODO
-"""
-Alchemy.js - clusterization and visualization of neo4j db
-http://nicolewhite.github.io/2014/07/24/visualize-subset-neo4j-alchemy.html
-http://graphalchemist.github.io/Alchemy/#/examples
-http://stackoverflow.com/questions/1545606/python-k-means-algorithm
-Dla zbioru 20000x20 zaproponować własny use case, np. w oparciu o metodę klasteryzacji k-means
-lub inną z bibliotek pythonowskich.
-
-https://www.cs.ucsb.edu/~xyan/classes/CS595D-2009winter/MCL_Presentation2.pdf
-http://jexp.de/blog/2014/03/quickly-create-a-100k-neo4j-graph-data-model-with-cypher-only/
-Erdős–Rényi model
-"""
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -31,66 +15,139 @@ gph = py2neo.Graph('http://localhost:7474/db/data')
 
 
 class DrawHelper:
-    def __init__(self, nodes, edges):
-        self.nodes = nodes
+
+    # @input VertexClustering object
+    # @output JSON
+    def __init__(self, clusters, edges):
+        self.nodes = []
         self.edges = edges
+
+        for idx, cluster in enumerate(clusters):
+            for node in cluster:
+                # print node.attributes()
+                self.nodes.append({
+                    'id': node,
+                    'name': 'node',
+                    'cluster': idx
+                })
 
     @property
     def serialize(self):
         return {
             "nodes": [
                 {
-                    "id": n.properties["id"],
-                    "name": n.properties["name"],
-                    "cluster": randrange(0, 4, 1)
+                    "id": n['id'],
+                    "name": n['name'],
+                    "cluster": n['cluster']
                 } for n in self.nodes],
             "edges": [
                 {
-                    "source": e.nodes[0].properties["id"],
-                    "target": e.nodes[1].properties["id"]
+                    "source": e[0],
+                    "target": e[1]
                 } for e in self.edges],
-
         }
+
+
+def reset_neo4j():
+    # remove all
+    gph.cypher.execute("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
+
+    # Added 500 labels, created 500 nodes, set 1000 properties, statement executed in 127 ms.
+    gph.cypher.execute(
+        """WITH ["audi", "bmw", "ford", "mercedes", "nissan", "opel", "peugeot", "renault", "toyota", "volkswagen"]
+         AS names FOREACH (r IN range(0,100) | CREATE (:Car {id:r, name:names[r % size(names)]+" "+r}));"""
+    )
+
+    # Added 10 labels, created 10 nodes, set 20 properties, statement executed in 41 ms.
+    gph.cypher.execute(
+        """WITH ["hybrid engine", "spare wheel", "computer", "digital speedometer", "sunroof",
+         "alarm", "abs", "esp", "asr", "muffler"] AS names FOREACH
+         (r IN range(0,9) | CREATE (:Part {id:r, name:names[r % size(names)]+" "+r}));"""
+    )
+
+    # Created 143 relationships, statement executed in 112 ms.
+    gph.cypher.execute("MATCH (c:Car),(p:Part) WITH c,p LIMIT 800 WHERE rand() < 0.1 CREATE (c)-[:HAS]->(p);")
+
+    # Added 1 index, statement executed in 36 ms.
+    gph.cypher.execute("create index on :Car(id);")
+
+    # Added 1 index, statement executed in 22 ms.
+    gph.cypher.execute("create index on :Part(id);")
 
 
 @app.route('/')
 def index():
+    return render_template('index.html')
 
-    # test = graph.find_one("Person", "born", 1951)  # MATCH (a:Person {born: 1951}) RETURN a
-    cars = gph.find("Car", "name", "renault", 150)
 
-    # g = igraph.Graph.Erdos_Renyi(500, 0.05, directed=True, loops=False)
+@app.route('/graph/<string:data_type>/<int:case>')
+def graph(data_type=None, case=None):
 
-    # igraph.Clustering.membership.
-    # igraph.Vertex.betweenness()
-    # cypher.
-    # print g
+    if data_type == "neo4j":
+        if case == 0:
+            data_source = "/data/neo4j/0"
+        elif case == 1534234:
+            reset_neo4j()
+            return redirect(url_for('index'))
+    elif data_type == "igraph":
+        data_source = '/data/igraph/'+str(case)
 
-    return render_template('index.html')  # , txt=str(g))
+    return render_template('graph.html', data_source=data_source)
 
-@app.route('/data/<string:data_type>')
-def data(data_type=None):
 
-    if data_type == "cars":
-        # cars = gph.find("Car", "name", "renault", 150)
-        # c = gph.cypher.execute("MATCH (p:Part)<-[r:HAS]-(c:Car) WHERE c.id IN
-        # [10,15,22,545,346,500,123,54,78,456,345,333,444,222,111] RETURN r LIMIT 100;")
-        # c = gph.cypher.execute("MATCH (c:Car)-[r:HAS]->(p:Part) RETURN r SKIP 1800 LIMIT 300 ")
-        c = gph.cypher.execute("MATCH (p:Part)<-[r:HAS]-(c:Car) WHERE p.id = 9 OR c.id = 100 OR c.id = 111 RETURN r")
-        d = c.to_subgraph()
-        # print str(dir(cars))
-        dh = DrawHelper(d.nodes, d.relationships)
+# data and graph is divided into two routes (instead of request.method modifier)
+# coz alchemy uses only GET :/
+@app.route('/data/<string:data_type>/<int:case>')
+def data(data_type=None, case=0):
+
+    if data_type == "neo4j":
+
+        if case == 0:
+            query_n = "MATCH (c:Car)-[:HAS]->(:Part) RETURN DISTINCT ID(c) AS id, c.name AS name"
+            query_e = "MATCH (c1:Car)-[:HAS]->(:Part)<-[:HAS]-(c2:Car) RETURN ID(c1) AS source, ID(c2) AS target"
+
+            nodes = gph.cypher.execute(query_n)
+            edges = gph.cypher.execute(query_e)
+
+            g = igraph.Graph()
+            for node in nodes:
+                g.add_vertex(name=str(node.id))
+            for edge in edges:
+                g.add_edge(str(edge.source), str(edge.target))
+
+            g = g.as_undirected()
+
+        else:
+            pass  # no other cases yet
+
+    elif data_type == "igraph":
+        if case == 0:
+            g = igraph.Graph.Erdos_Renyi(50, 0.05, directed=False, loops=False)
+        elif case == 1:
+            g = igraph.Graph.K_Regular(50, 6, directed=False, multiple=False)
+        elif case == 2:
+            g = igraph.Graph.Full(40, directed=False, loops=False)
+        elif case == 3:
+            g = igraph.Graph.Forest_Fire(80, 0.1, bw_factor=0.0, ambs=1, directed=False)
+        elif case == 4:
+            g = igraph.Graph.Barabasi(80, 2)
+
+    # communities = g.community_multilevel()
+    clusters = g.community_edge_betweenness().as_clustering()
+
+    # membership vector
+    # membership = clusters.membership
+
+    if not g:
+        dh = {
+            "nodes": [],
+            "edges": []
+        }
     else:
-        dh = DrawHelper([], [])
+        dh = DrawHelper(clusters, g.get_edgelist())
 
     return jsonify(dh.serialize)
 
-
-@app.route('/generate/<int:modifier>')
-def generate(modifier=0):
-
-    if modifier:
-        pass
 
 
 if __name__ == '__main__':
