2666aea6d82aee554ea763d208b4bd3fff7e0ad0
Grzegorz Wójcicki <grzegorz-wojcicki@outlook.com>
2015-04-27 15:48:36 +0200

    Random clusters

--- README.md
+++ README.md
@@ -26,11 +26,36 @@ python2 app.py
 
 ```neo4j
 
-WITH ["audi", "bmw", "form", "mercedes", "nissan", "opel", "peugeot", "renault", "toyota", "wolksvagen"]
+// remove all
+MATCH (n)
+OPTIONAL MATCH (n)-[r]-()
+DELETE n,r
+
+WITH ["audi", "bmw", "ford", "mercedes", "nissan", "opel", "peugeot", "renault", "toyota", "volkswagen"]
 AS names FOREACH (r IN range(0,20000) | CREATE (:Car {id:r, name:names[r % size(names)]+" "+r}));
 
-WITH ["engine", "wheels", "dashboard", "speedometer", "sunroof", "alarm", "abs", "esp", "asr", "muffler"]
-AS names FOREACH (r IN range(0,100) | CREATE (:Part {id:r, name:names[r % size(names)]+" "+r}));
+// Added 20001 labels, created 20001 nodes, set 40002 properties, statement executed in 632 ms.
 
-// Added 20001 labels, created 20001 nodes, set 40002 properties, statement executed in 20211 ms.
+WITH ["hybrid engine", "spare wheel", "computer", "digital speedometer", "sunroof", "alarm", "abs", "esp", "asr", "muffler"]
+AS names FOREACH (r IN range(0,9) | CREATE (:Part {id:r, name:names[r % size(names)]+" "+r}));
+
+// Added 101 labels, created 101 nodes, set 202 properties, statement executed in 65 ms.
+
+MATCH (c:Car),(p:Part) WITH c,p
+LIMIT 150000 WHERE rand() < 0.1
+CREATE (c)-[:HAS]->(p);
+
+// Created 15136 relationships, statement executed in 659 ms.
+
+create index on :Car(id);
+// Added 1 index, statement executed in 36 ms.
+
+create index on :Part(id);
+// Added 1 index, statement executed in 22 ms.
+
+
+MATCH (p:Part)<-[r:HAS]-(c:Car) WHERE p.id = 9 OR c.id = 100 OR c.id = 111 RETURN r; // duzo!
+MATCH (p:Part)<-[r:HAS]-(c:Car) WHERE c.id IN [10,15,22,545,346,500,123,54,78,456,345,333,444,222,111] RETURN r LIMIT 100;
+
+
 ```
--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 __author__ = 'rikkt0r'
 
-from flask import Flask, render_template
-from py2neo import Graph, Node, Relationship, authenticate
+from flask import Flask, render_template, jsonify
+import py2neo
+import igraph
 import os
+from random import randrange
+
 
 
 # TODO
@@ -16,34 +19,77 @@ Dla zbioru 20000x20 zaproponować własny use case, np. w oparciu o metodę klas
 lub inną z bibliotek pythonowskich.
 
 https://www.cs.ucsb.edu/~xyan/classes/CS595D-2009winter/MCL_Presentation2.pdf
+http://jexp.de/blog/2014/03/quickly-create-a-100k-neo4j-graph-data-model-with-cypher-only/
 Erdős–Rényi model
 """
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-authenticate('localhost:7474', 'neo4j', 'qwerty')
-graph = Graph('http://localhost:7474/db/data')
+py2neo.authenticate('localhost:7474', 'neo4j', 'qwerty')
+gph = py2neo.Graph('http://localhost:7474/db/data')
+
+
+class DrawHelper:
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
+
+    @property
+    def serialize(self):
+        return {
+            "nodes": [
+                {
+                    "id": n.properties["id"],
+                    "name": n.properties["name"],
+                    "cluster": randrange(0, 4, 1)
+                } for n in self.nodes],
+            "edges": [
+                {
+                    "source": e.nodes[0].properties["id"],
+                    "target": e.nodes[1].properties["id"]
+                } for e in self.edges],
+
+        }
 
 
 @app.route('/')
 def index():
 
-    test = graph.find_one("Person", "born", 1951)  # MATCH (a:Person {born: 1951}) RETURN a
-    print test
+    # test = graph.find_one("Person", "born", 1951)  # MATCH (a:Person {born: 1951}) RETURN a
+    cars = gph.find("Car", "name", "renault", 150)
 
-    return render_template('index.html', txt=test)
+    # g = igraph.Graph.Erdos_Renyi(500, 0.05, directed=True, loops=False)
 
-@app.route('generate/<int:modifier>')
+    # igraph.Clustering.membership.
+    # igraph.Vertex.betweenness()
+    # cypher.
+    # print g
+
+    return render_template('index.html')  # , txt=str(g))
+
+@app.route('/data/<string:data_type>')
+def data(data_type=None):
+
+    if data_type == "cars":
+        # cars = gph.find("Car", "name", "renault", 150)
+        # c = gph.cypher.execute("MATCH (p:Part)<-[r:HAS]-(c:Car) WHERE c.id IN
+        # [10,15,22,545,346,500,123,54,78,456,345,333,444,222,111] RETURN r LIMIT 100;")
+        c = gph.cypher.execute("MATCH (c:Car)-[r:HAS]->(p:Part) RETURN r SKIP 1800 LIMIT 300 ")
+        d = c.to_subgraph()
+        # print str(dir(cars))
+        dh = DrawHelper(d.nodes, d.relationships)
+    else:
+        dh = DrawHelper([], [])
+
+    return jsonify(dh.serialize)
+
+
+@app.route('/generate/<int:modifier>')
 def generate(modifier=0):
 
     if modifier:
         pass
-
-
-@app.route('/mockup')
-def mockup():
-    return render_template('index.html')
 
 
 if __name__ == '__main__':
